845a9b22d1e237c4a3ced0997343313a0ee96ec4
Gavriel R-H <gavriel@gavrielrh.com>
2023-04-22 00:09:52 +0000

    Fix up links for schema files

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ async function run() {
     // since we're a static site, we need to use the github api to find out what files we have access to :(
     const data = await fetch('https://api.github.com/repos/gavrielrh/tea-db/git/trees/main').then(res => res.json());
     const files = Object
-        .entries(data.tree)
+        .values(data.tree)
         // We want to only include .json files that aren't the schema.
         .filter(file => file.path?.endsWith('.json') && file.path !== "tea.schema.json");
     for (const file of files) {
         const contents = await fetch(file.path).then(res => res.json());
         // All tea json files should render as list item children of the tea list
         const teaListItem = document.createElement('li');
-        teaListItem.textContent = contents.name;
+        const teaLink = document.createElement('a');
+        teaLink.href = file.path;
+        teaLink.textContent = contents.name;
+        teaListItem.appendChild(teaLink);
         teaList.appendChild(teaListItem);
     }
     if (files.length > 0) {
